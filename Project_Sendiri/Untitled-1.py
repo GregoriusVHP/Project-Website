3dcf79015c4d50c53e35a8e53a53a637984b926d
GregoriusVHP <37.gregorius.prasetyo@gonzaga.sch.id>
2023-12-03 09:30:19 +0700

    Update Untitled-1.py

--- Project_Sendiri/Untitled-1.py
+++ Project_Sendiri/Untitled-1.py
@@ -1,3 +1,8 @@
+import math
+'''
+PROJECT BY GREGORIUS VALERY HERMAWAN PRASETYO.
+UNTUK TUGAS INDIVIDU TIK.
+'''
 def main():
     print("-"*25)
     print(" MENGHITUNG BANGUN RUANG")
@@ -7,24 +12,76 @@ def main():
 
     if x == 1:
         hitung_Balok()
+    elif x == 2:
+        hitung_kerucut()
+    elif x == 3:
+        hitung_bola()
     else:
-        pass
+        print("Terimakasih telah mencoba program ini :)")
 
 def hitung_Balok():
     print()
     print("+"*25)
     print(" MENGHITUNG BALOK")
     print("+"*25)
-    lebar = int(input("Masukan lebar: "))
-    tinggi = int(input("Masukan tinggi: "))
-    panjang = int(input("Masukan panjang: "))
+    lebar = float(input("Masukan lebar: "))
+    tinggi = float(input("Masukan tinggi: "))
+    panjang = float(input("Masukan panjang: "))
 
     volume = panjang*lebar*tinggi
     Lp = (2*((panjang*lebar) + (panjang*tinggi) + (lebar*tinggi)))
 
-    print("Volume = {} \nLuas permukaan = {}"  .format(volume, Lp))
+    v = int(volume)
+    lp = int(Lp)
+    print("Volume = {} \nLuas permukaan = {}"  .format(v, lp))
+    print()
+    loop()
 
-        
+def hitung_kerucut():
+    print()
+    print("+"*25)
+    print(" MENGHITUNG KERUCUT")
+    print("+"*25)
+    r = float(input("Masukan jari-jari: "))
+    t = float(input("Masukan tinggi: "))
+    s = math.sqrt((r**2)+(t**2))
 
+    pi = 3.14
+    volume = 1/3 * pi * r**2 * t
+    Lp = pi * r * (r+s)
+    v=int(volume)
+    lp=int(Lp)
+    print("Volume = {} \nLuas permukaan = {}"  .format(v, lp))
+    print()
+    loop()
+
+def hitung_bola():
+    print()
+    print("+"*25)
+    print(" MENGHITUNG BOLA ")
+    print("+"*25)
+    r = float(input("Masukan jari-jari: "))
+    t = float(input("Masukan tinggi: "))
+
+    pi = 3.14
+    volume = 4/3 * pi * r**3
+    Lp = 4 * pi * r**2
+
+    v=int(volume)
+    lp=int(Lp)
+    print("Volume = {} \nLuas permukaan = {}"  .format(v, lp))
+    print()
+    loop()
+
+def loop():
+    y = True
+    while y == True:
+        x = input("Mau ulangi lagi?(Y/N) ")
+
+        if x == "Y" or x == "y":
+            main()
+            print("\n")
+        else:
+            print("\nTerimakasih telah mencoba program ini :)")
+            break
 main()
-
