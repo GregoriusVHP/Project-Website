8e446ac78430fab59cc2621af6f1bae305010216
GregoriusVHP <142484992+GregoriusVHP@users.noreply.github.com>
2024-01-23 15:55:37 +0700

    Add files via upload

--- WEB2/index.php
+++ WEB2/index.php
@@ -3,10 +3,18 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>FOMOgram</title>
         <link rel="stylesheet" href="stylesheet.css">
     </head>
     <body>
-        <
+        <heading>
+        <div class="sidebar">
+            <a href="" class="logo"><img src="FOMO/2.png" alt="Logo"></a>
+        </div>
+        <div class="main-home"></div>
+        </div>
+        </heading>
+        
     </body>
 </html>
