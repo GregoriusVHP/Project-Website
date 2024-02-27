e3f6f8321d7e968546affdc4a6031b5c6d1aad9e
GregoriusVHP <142484992+GregoriusVHP@users.noreply.github.com>
2024-02-27 16:04:50 +0700

    Update index.php

--- WEB2/index.php
+++ WEB2/index.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Responsive Instagram</title>
     <link rel="stylesheet" href="stylesheet.css">
+    <?php 
+    $post_list=[];
+    $post1=new stdClass();
+    $post1->Username="Boss3y";
+    $post1->image="img1.jpg";
+    $post1->cite="Apa aja lah";
+    $post_list[]=$post1;?>
 </head>
 <body>
     <header>
@@ -17,42 +24,17 @@
             </ul>
         </nav>
     </header> 
-    <?php 
-    $post_list=[];
-    $post1=new stdClass();
-    $post1->caption="caption";
-    $post1->image="image.jpg";
-    $post_list[]=$post1;
-
+    <?php
     for($i=0;$i<count($post_list);$i++){
         echo "<div class='tm-post'>";
-        echo "<h2>" .$post_list[$i] -> caption."</h2>";
+        echo "<h2>" .$post_list[$i] -> Username."</h2>";
         echo "<img src=". $post_list[$i]-> image. ">";
-        echo "</div";
+        echo "<p>". $post_list[$i]->cite."</p>";
+        echo "</div>";
     }?>
-        <div class="post">
-            <img src="img2.jpg  " alt="Post Image">
-            <div class="post-details">
-                <h2>Post Title</h2>
-                <p>Posted by User123</p>
-                <p>monkey</p>
-            </div>
-        </div>
-        <!-- More posts... -->
-        <div class="post">
-            <img src="img1.jpg  " alt="Post Image">
-            <div class="post-details">
-                <h2>Post Title</h2>
-                <p>Posted by User123</p>
-                <p>Car.</p>
-            </div>
-        </div>
-   
-
     <footer>
         <p>&copy; 2024 Instagram</p>
     </footer>
 </body>
 </html>
-
 
