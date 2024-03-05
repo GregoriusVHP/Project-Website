63673113329f004f5e6df22e3b160a6edfd2fda1
GregoriusVHP <142484992+GregoriusVHP@users.noreply.github.com>
2024-03-05 16:01:21 +0700

    Update index.php

--- WEB2/index.php
+++ WEB2/index.php
@@ -11,11 +11,23 @@
     $post1->Username="Boss3y";
     $post1->image="img1.jpg";
     $post1->cite="Apa aja lah";
-    $post_list[]=$post1;?>
+    $post_list[]=$post1;
+    $post2=new stdClass();
+    $post2->Username="Boss3y";
+    $post2->image="img2.jpg";
+    $post2->cite="Apa aja lah";
+    $post_list[]=$post2;
+    $post3=new stdClass();
+    $post3->Username="Boss3y";
+    $post3->image="img3.jpg";
+    $post3->cite="Apa aja lah";
+    $post_list[]=$post3;?>
 </head>
 <body>
     <header>
-        <div class="logo"><img src="FOMO/2.png"></div>
+        <div class="logo">
+            <img src="Profile.jpg">
+        </div>
         <nav>
             <ul>
                 <li><a href="#">Home</a></li>
@@ -24,17 +36,18 @@
             </ul>
         </nav>
     </header> 
+    <br><br>
     <?php
     for($i=0;$i<count($post_list);$i++){
-        echo "<div class='tm-post'>";
+        echo "<div class='post' id='post'>";
         echo "<h2>" .$post_list[$i] -> Username."</h2>";
         echo "<img src=". $post_list[$i]-> image. ">";
         echo "<p>". $post_list[$i]->cite."</p>";
         echo "</div>";
+        echo "<br>";
     }?>
     <footer>
-        <p>&copy; 2024 Instagram</p>
-    </footer>
+         Blog 207
+</footer>
 </body>
 </html>
-
